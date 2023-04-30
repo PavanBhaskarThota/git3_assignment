e11839da24bc8005d6f10dad0537cdb0e6b916bf
Shiva kumar <shivalucky0202@gmail.com>
2023-04-30 21:35:51 +0530

    prime check

--- primeCode.js
+++ primeCode.js
@@ -1,14 +1,11 @@
-function checkPrime(a){
-
-if(a<=0)
-return false;
-
-for(let i=2; i<Math.sqrt(a); i++){
-   if(a%i===0)
-   return false;
-}
- return true;
-
-}
-
-checkPrime ? console.log('prime') : console.log('Not prime);
+ count=0;
+    for(let i=1; i<=num; i++){
+     if (num%i==0)
+            count++;
+    }
+    if(count==2){
+            console.log("Yes");
+    }
+    else{
+            console.log("No");
+    }
